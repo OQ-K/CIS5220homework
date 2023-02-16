ac23afc92b2c579ccdad09597c92aa93f8994c9f
Olivia Q <jiarongq00@gmail.com>
2023-02-16 17:27:36 -0500

    hw3 commit

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,21 +6,27 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 50
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "num_batches": 782,
+        "num_epochs": num_epochs,
+        "initial_learning_rate": initial_learning_rate,
+        "eta_max": 0.003,
+        "gamma": 0.98,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
