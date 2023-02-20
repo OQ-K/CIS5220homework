c0f4fc7886d9f8e79df02c6dcd0b1db609b724e4
Olivia Q <jiarongq00@gmail.com>
2023-02-20 08:13:35 -0500

    hw3 last commit

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -17,7 +17,7 @@ class CONFIG:
         "num_batches": 900,
         "num_epochs": num_epochs,
         "initial_learning_rate": initial_learning_rate,
-        "eta_max": 0.002,
+        "eta_max": 0.0029,
         "gamma": 0.98,
     }
 
